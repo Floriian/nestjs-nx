10b106a66fd6072ab939b66b2060b2c84153c895
Floriian <lovasifloriian@outlook.hu>
2023-12-25 15:46:55 +0100

    feat: create user entity

--- apps/api/src/app/users/entities/user.entity.ts
+++ apps/api/src/app/users/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import {
+  BeforeCreate,
+  BeforeUpdate,
+  Column,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import argon2 from 'argon2';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Table
+export class User extends Model {
+  @ApiProperty()
+  @Column
+  email: string;
+
+  @ApiProperty()
+  @Column
+  password: string;
+
+  @ApiProperty()
+  @Column
+  token: string;
+
+  @BeforeCreate
+  @BeforeUpdate
+  static async hashFields(user: User) {
+    user.password = await argon2.hash(user.password);
+    user.token = await argon2.hash(user.token);
+  }
+}
