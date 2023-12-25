8bd9b5be32e8276937d3f3c972adca347051d927
Floriian <lovasifloriian@outlook.hu>
2023-12-26 00:29:13 +0100

    feat: add public decorator.

--- apps/api/src/app/auth/auth.controller.ts
+++ apps/api/src/app/auth/auth.controller.ts
@@ -3,17 +3,20 @@ import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 @ApiTags('Auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('/sign-in')
   signIn(@Body() dto: SignInDto) {
     return this.authService.signIn(dto);
   }
 
+  @Public()
   @Post('/sign-up')
   signUp(@Body() dto: SignUpDto) {
     return this.authService.signUp(dto);
