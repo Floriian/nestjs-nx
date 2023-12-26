eb1408e89eddb49e21bd204e213bd03879c2d66e
Floriian <lovasifloriian@outlook.hu>
2023-12-26 15:49:34 +0100

    typeorm migration try one.

--- apps/api/src/app/config/typeorm.config.ts
+++ apps/api/src/app/config/typeorm.config.ts
@@ -1,6 +1,8 @@
 import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 import { EnvModule } from '../env/env.module';
 import { EnvService } from '../env/env.service';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
 export const typeormConfig: TypeOrmModuleAsyncOptions = {
   imports: [EnvModule],
   inject: [EnvService],
@@ -15,3 +17,19 @@ export const typeormConfig: TypeOrmModuleAsyncOptions = {
     synchronize: true,
   }),
 };
+
+export const buildDataSource = (envService: EnvService): DataSourceOptions => {
+  return {
+    host: envService.get('DATABASE_HOST'),
+    port: envService.get('DATABASE_PORT'),
+    username: envService.get('DATABASE_USER'),
+    password: envService.get('DATABASE_PASSWORD'),
+    database: envService.get('DATABASE_DB'),
+    type: 'postgres',
+    synchronize: true,
+  };
+};
+
+export default new DataSource(
+  buildDataSource(new EnvService(new ConfigService()))
+);
