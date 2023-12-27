704220bd6d278e493a3f3a9eaeae6e50c88ccd2f
Floriian <lovasifloriian@outlook.hu>
2023-12-28 00:06:01 +0100

    Revert "feat: add api tags for user"
    This reverts commit 1fd9d53b331ed84f31fbd362b0721dcc8960c91d.

--- apps/api/src/app/users/users.controller.ts
+++ apps/api/src/app/users/users.controller.ts
@@ -1,10 +1,8 @@
 import { Controller, Get, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
-@ApiTags('Users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
   @Get()
